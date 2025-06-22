8e9e74245db60688c5eb2f61aff75597be119fab
Vijay <vijay99sankar@gmail.com>
2025-06-22 11:14:38 +0200

    removed unnecessary code

--- src/demo.py
+++ src/demo.py
@@ -5,38 +5,6 @@ import plotly.express as px
 from dt_modules import BarChart, LineChart, PieChart, Table, ScatterPlot, Histogram, BoxPlot, HeatMap
 
 
-def start_old():
-    data = {
-        'sectoren': ['A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J', 'K', 'L', 'M', 'N', 'O', 'P', 'Q', 'R', 'S', 'T', 'U'],
-        'uitstroom': [63589, 7615, 689794, 22133, 34755, 335913, 894066, 304780, 162482, 198830, 234537, 57592, 409602, 493306, 472004, 398288, 884573, 74078, 103002, 12192, 630],
-        'sector_namen': get_sectors(),
-    }
-
-    df = pd.DataFrame(data)
-
-    bar_chart = BarChart(df, x="sectoren", y="uitstroom", column_to_color="sectoren")
-    bar_chart.save_json("./exports/bar_chart.export.json")
-
-    pie_chart = PieChart(data=df, values="uitstroom", names="sectoren")
-    pie_chart.save_json("./exports/pie_chart.export.json")
-
-    headers = ["Letters", "Beschrijvingen"]
-    cells = [df["sectoren"].head(5), df["sector_namen"].head(5)]
-    table = Table(headers=headers, cells=cells, alternate_row=True)
-    table.save_json("./exports/table.export.json")
-
-    scatter = ScatterPlot(x=[0, 1, 2, 3, 4], y=[0, 1, 4, 9, 16])
-    scatter.save_json("./exports/scatter.export.json")
-
-    df = px.data.tips()
-    histogram = Histogram(df, x="total_bill", nbins=10, title="Rekening en kosten", y_label="cost")
-    histogram.save_json("./exports/histogram.export.json")
-
-    df = px.data.tips()
-    box_plot = BoxPlot(df, x="time", column_to_color="time", y="total_bill")
-    box_plot.save_json("./exports/box_plot.export.json")
-
-
 def start():
     data = {
         'sectoren': ['A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J', 'K', 'L', 'M', 'N', 'O', 'P', 'Q', 'R', 'S', 'T', 'U'],
@@ -50,7 +18,6 @@ def start():
     bar_df = df.sort_values(by=["uitstroom"])
     bar_chart = BarChart(bar_df.head(5), x="sectoren", y="uitstroom", column_to_color="sectoren")
     bar_chart.save_json_v2("./exports/bar_chart.export.json")
-    # bar_chart.save_json("./exports/bar_chart.export.json")
 
     # Pie
     pie_chart = PieChart(data=df, values="uitstroom", names="sectoren")
@@ -65,29 +32,25 @@ def start():
     # Scatter
     scatter_df = pd.DataFrame(data={"x": [0, 1, 2, 3, 4], "y": [0, 1, 4, 9, 16]})
     scatter = ScatterPlot(scatter_df, x="x", y="y")
-    # scatter.get_figure().show()
     scatter.save_json_v2("./exports/scatter.export.json")
-    scatter.data.to_excel("exports/scatter.export.xlsx")
+    # scatter.data.to_excel("exports/scatter.export.xlsx")
 
     # Histogram
     df = px.data.tips()
     histogram = Histogram(df, x="total_bill", nbins=10, title="test title", y_label="cost", colors=["#CA005D"])
     histogram.save_json_v2("./exports/histogram.export.json")
-    histogram.data.to_excel("exports/histogram.export.xlsx")
+    # histogram.data.to_excel("exports/histogram.export.xlsx")
 
     # Boxplot
     box_plot = BoxPlot(df, x="time", column_to_color="time", y="total_bill")
-    # box_plot.get_figure().show()
     box_plot.save_json_v2("./exports/box_plot.export.json")
     box_plot.data.to_excel("exports/box_plot.export.xlsx")
 
     heatmap_data = get_heatmap_data()
     df = pd.DataFrame(heatmap_data)
-    # heatmap = HeatMap(df, x="nation", y="medal", value_column="count")
     heatmap = HeatMap(df, x="provincie", y="sector", value_column="werkloosheid")
     heatmap.save_json_v2("exports/heatmap.export.json")
     heatmap.data.to_excel("exports/heatmap.export.xlsx")
-    # heatmap.get_figure().show()
 
 def get_heatmap_data():
     return [
